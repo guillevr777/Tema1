8c29c0d2981aa654ee077e04e1b34546d2bfebfe
gs.villanueva <gs.villanueva@106-29.info2>
2024-09-23 12:55:39 +0200

    update

--- Tema1/src/Parte1/ejer2.java
+++ Tema1/src/Parte1/ejer2.java
@@ -1,5 +1,23 @@
 package Parte1;
 
-public class ejer2 {
+import java.util.Scanner;
 
+public class ejer2 {
+	
+	public static void main(String[] args) {
+		//creamos el escaner
+		Scanner reader = new Scanner(System.in);
+		//variable para almacenar numero
+		int number;
+		//pedimos numero al usuario
+		System.out.println("introce tu edad");
+		//lee el numero en consola 
+		number = reader.nextInt();
+		//le pedimos que sume un año
+		int a = 1;
+		//da el numero que dimos
+		System.out.print("El año que viene tendras " + number);
+		//se cierra el escaner
+		reader.close();
+	}
 }
