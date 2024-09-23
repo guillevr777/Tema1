7251827c282d82a02bce154c45231453991f01cd
gs.villanueva <gs.villanueva@106-29.info2>
2024-09-23 12:39:37 +0200

    Done

--- Tema1/src/Parte1/ejer1.java
+++ Tema1/src/Parte1/ejer1.java
@@ -1,5 +1,21 @@
 package Parte1;
 
-public class ejer1 {
+import java.util.Scanner;
 
+public class ejer1 {
+	
+	public static void main(String[] args) {
+		//creamos el escaner
+		Scanner reader = new Scanner(System.in);
+		//variable para almacenar numero
+		int number;
+		//pedimos numero al usuario
+		System.out.println("introduzca un numero");
+		//lee el numero en consola 
+		number = reader.nextInt();
+		//da el numero que dimos
+		System.out.print("Has elegido " + number + " como numero.");
+		//se cierra el escaner
+		reader.close();
+	}
 }
