0ea4679e79fe12834c3029a56a8ead064df1eea3
guill <guill@DESKTOP-0V4E8HS>
2024-09-24 15:47:36 +0200

    .

--- Tema1/src/Parte1/ejer10.java
+++ Tema1/src/Parte1/ejer10.java
@@ -1,5 +1,34 @@
 package Parte1;
 
-public class ejer10 {
+import java.util.Scanner;
 
+public class ejer10 {
+	
+	public static void main(String[] args) {
+		
+		//hacemos las variables
+			
+		int numero;
+		boolean overage;
+		
+		//creamos el escaner
+		
+		Scanner reader = new Scanner(System.in);
+		
+		//preguntamos por la edad
+		
+		System.out.println("Di un numero");
+		
+		numero = reader.nextInt();
+		
+		//usamos relacionales , si la edad es supeior a 18 true sino false
+		
+		overage = numero % 2 == 0;
+		
+		//pone la respuesta en pantalla
+		
+		System.out.print(overage);
+			
+		reader.close();
+		}
 }
