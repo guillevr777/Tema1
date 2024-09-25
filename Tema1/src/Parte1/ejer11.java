73d829a1c75eabb5853061b94803d828e066a299
gs.villanueva <gs.villanueva@106-29.info2>
2024-09-25 10:14:16 +0200

    More

---
+++ Tema1/src/Parte1/ejer11.java
@@ -0,0 +1,37 @@
+package Parte1;
+
+import java.util.Scanner;
+
+public class ejer11 {
+	
+	public static void main(String[]args) {
+		//presentamos las variables
+		double peseta = 166;
+		double euro = 1.00;
+		//creamos el escaner
+		Scanner reader = new Scanner(System.in);
+		//le ponemos al usuario que diga una cantidad de pesetas
+		System.out.println("Dime una cantidad de pesetas");
+		//lee las pesetas
+		peseta = reader.nextDouble();
+		
+		//dice cuanto es la conversion
+		System.out.println( euro + " $ = " + peseta + " pstas");	
+		
+
+		
+		
+		
+		
+		
+		
+		
+
+	
+	
+	
+	
+	
+	
+	}
+}
