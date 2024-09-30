387eb4d030519c8082a314986eebefd39ae6908f
gs.villanueva <gs.villanueva@106-29.info2>
2024-09-30 12:53:45 +0200

    a

--- Tema1/src/parte2/ejer18.java
+++ Tema1/src/parte2/ejer18.java
@@ -1,10 +1,26 @@
 package parte2;
 
+import java.util.Scanner;
+
 public class ejer18 {
 	public static void name(String[]args) {
 		//creamos el escaner
 		Scanner reader = new Scanner();
 		//creamos las variables
+		double num1;
+		double num2;
+		double num3;
+		//le preguntamos por los numerillos
+		System.out.println("Di un numero joder ostia puta");
+		//hacemos las operacion
+		num1 = reader.nextDouble();
+		//le preguntamos por los numerillos
+		System.out.println("Di un numero joder ostia puta");
+		//hacemos las operacion
+		num2 = reader.nextDouble();
+		//Hacemos las operaciones
+		
+		
 		
 	}
 }
