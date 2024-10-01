f6746fd949142735036f372f6025831ba85b179f
gs.villanueva <gs.villanueva@106-29.info2>
2024-10-01 14:19:39 +0200

    Ejercicios hechos del 5 al 10

--- Tema1/src/parte2/ejer8.java
+++ Tema1/src/parte2/ejer8.java
@@ -6,14 +6,13 @@ public class ejer8 {
 	public static void main(String[]args) {
 		//creamos el escaner
 		Scanner reader = new Scanner(System.in);
-		//Creamos las variables
+		//Creamos las variables para identificar los centimetros y metros
 		double centimetros;
 		double metros;
-		int resultado;
-		//Pedimos numero de personas total
+		//Pedimos numero de metros
 		System.out.println("Dime a cuantos metros ha llegado");
 		metros = reader.nextDouble();
-		//calculamos
+		//calculamos la conversion de metros a centimetros y lo truncamos
 		centimetros = (int) (metros*100);
 		//mostramos el resultado
 		System.out.println("Seran unos : " + centimetros);
